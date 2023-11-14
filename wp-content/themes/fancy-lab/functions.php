a2d84639f5721602271a721ceaadf790ee9be584
Hendra Sutrisno <hendratrisno08@gmail.com>
2023-11-14 19:36:08 +0700

    menampilkan blog pada template-home.php

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -73,6 +73,8 @@ function fancy_lab_config(){
 		) );
 
 		add_image_size('fancy-lab-slider', 1920, 800, array('center', 'center'));
+		add_image_size('fancy-lab-blog', 960, 640, array('center', 'center'));
+
 
 		if ( ! isset( $content_width ) ) {
 			$content_width = 600;
